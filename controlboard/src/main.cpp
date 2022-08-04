1820deb6e0c0f44b0c04ac691f7a19475c11d8a6
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-08-04 21:27:36 +0200

    feat: GPS NMEA module POC

--- controlboard/src/main.cpp
+++ controlboard/src/main.cpp
@@ -1,4 +1,8 @@
 #include "Arduino.h"
+#include "TinyGPS++.h"
+
+HardwareSerial Serial3(PB11, PB10);
+TinyGPSPlus gps;
 
 #define PIN_INDICATOR_LED PC13
 #define PIN_RACK_LAMP PA8
@@ -6,18 +10,23 @@
 void setup() {
   pinMode(PIN_INDICATOR_LED, OUTPUT);
   pinMode(PIN_RACK_LAMP, OUTPUT);
+  Serial3.begin(9600);
 }
 
-
 void loop() {
-  digitalWrite(PIN_INDICATOR_LED, HIGH);
-  for(uint32_t v = 0;v < 255; v++) {
-    analogWrite(PIN_RACK_LAMP, v);
-    delay(8);
+  while (Serial3.available() > 0) {
+    char c = Serial3.read();
+    gps.encode(c);
   }
-  digitalWrite(PIN_INDICATOR_LED, LOW);
-  for(uint32_t v = 255;v > 0; v--) {
-    analogWrite(PIN_RACK_LAMP, v);
-    delay(8);
+
+  int hour = gps.time.hour();
+  int minute = gps.time.minute();
+  int second = gps.time.second();
+
+  if(second % 2 == 0) {
+    digitalWrite(PIN_INDICATOR_LED, HIGH);
+  } else {
+    digitalWrite(PIN_INDICATOR_LED, LOW);
   }
+  analogWrite(PIN_RACK_LAMP, second * 2);
 }
