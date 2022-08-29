d599cec450a3dff17918c89989eadedf262613a3
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-08-29 20:43:55 +0200

    feat: reading from DCF77 sensor

--- controlboard/src/main.cpp
+++ controlboard/src/main.cpp
@@ -1,15 +1,16 @@
 #include "Arduino.h"
 
 #define PIN_INDICATOR_LED PC13
+#define PIN_DCF77_TIME_IN PB9
 
 void setup() {
   pinMode(PIN_INDICATOR_LED, OUTPUT);
+  pinMode(PIN_DCF77_TIME_IN, INPUT);
 }
 
+int dcf77Lvl;
 void loop() {
-  delay(200);
-  digitalWrite(PIN_INDICATOR_LED, HIGH);
-  delay(200);
-  digitalWrite(PIN_INDICATOR_LED, LOW);
-
+  dcf77Lvl = digitalRead(PIN_DCF77_TIME_IN);
+  digitalWrite(PIN_INDICATOR_LED, dcf77Lvl);
+  delay(5);
 }
