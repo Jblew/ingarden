69c74858602237bc41fd3ab97840538187030482
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-08-29 20:20:22 +0200

    feat: blackpill STM32F401CE demo

--- controlboard/src/main.cpp
+++ controlboard/src/main.cpp
@@ -1,32 +1,15 @@
 #include "Arduino.h"
-#include "TinyGPS++.h"
-
-HardwareSerial Serial3(PB11, PB10);
-TinyGPSPlus gps;
 
 #define PIN_INDICATOR_LED PC13
-#define PIN_RACK_LAMP PA8
 
 void setup() {
   pinMode(PIN_INDICATOR_LED, OUTPUT);
-  pinMode(PIN_RACK_LAMP, OUTPUT);
-  Serial3.begin(9600);
 }
 
 void loop() {
-  while (Serial3.available() > 0) {
-    char c = Serial3.read();
-    gps.encode(c);
-  }
+  delay(200);
+  digitalWrite(PIN_INDICATOR_LED, HIGH);
+  delay(200);
+  digitalWrite(PIN_INDICATOR_LED, LOW);
 
-  int hour = gps.time.hour();
-  int minute = gps.time.minute();
-  int second = gps.time.second();
-
-  if(second % 2 == 0) {
-    digitalWrite(PIN_INDICATOR_LED, HIGH);
-  } else {
-    digitalWrite(PIN_INDICATOR_LED, LOW);
-  }
-  analogWrite(PIN_RACK_LAMP, second * 2);
 }
