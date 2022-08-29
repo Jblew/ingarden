eb2caae11ff74e3b85c566ace9a1741b49df2191
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-08-29 21:29:19 +0200

    feat: SWD debug on DCF77 module time

--- controlboard/src/main.cpp
+++ controlboard/src/main.cpp
@@ -1,16 +1,41 @@
 #include "Arduino.h"
+#include "DCF77.h"
+#include "TimeLib.h"
 
 #define PIN_INDICATOR_LED PC13
 #define PIN_DCF77_TIME_IN PB9
 
-void setup() {
+time_t time;
+DCF77 DCF = DCF77(
+    PIN_DCF77_TIME_IN,
+    digitalPinToInterrupt(PIN_DCF77_TIME_IN));
+
+void setup()
+{
   pinMode(PIN_INDICATOR_LED, OUTPUT);
   pinMode(PIN_DCF77_TIME_IN, INPUT);
+  Serial.begin(9600);
+  DCF.Start();
 }
 
-int dcf77Lvl;
-void loop() {
-  dcf77Lvl = digitalRead(PIN_DCF77_TIME_IN);
-  digitalWrite(PIN_INDICATOR_LED, dcf77Lvl);
-  delay(5);
+void loop()
+{
+  delay(1000);
+  time_t DCFtime = DCF.getTime();
+  if (DCFtime != 0)
+  {
+    Serial.println("Time is updated");
+    setTime(DCFtime);
+
+    // For ST-LINK + SWD debug
+    volatile int t_hour = 0;
+    volatile int t_minute = 0;
+    volatile int t_second = 0;
+    t_hour = hour();
+    Serial.println(t_hour);
+    t_minute = minute();
+    Serial.println(t_minute);
+    t_second = second();
+    Serial.println(t_second);
+  }
 }
