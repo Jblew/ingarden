443e92e5b25a7c7f1de1cb499e70ac43b579d221
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-09-01 20:47:32 +0200

    wip: DCF77 diagnostics

--- controlboard/src/main.cpp
+++ controlboard/src/main.cpp
@@ -1,41 +1,34 @@
 #include "Arduino.h"
-#include "DCF77.h"
-#include "TimeLib.h"
 
 #define PIN_INDICATOR_LED PC13
 #define PIN_DCF77_TIME_IN PB9
-
-time_t time;
-DCF77 DCF = DCF77(
-    PIN_DCF77_TIME_IN,
-    digitalPinToInterrupt(PIN_DCF77_TIME_IN));
 
 void setup()
 {
   pinMode(PIN_INDICATOR_LED, OUTPUT);
   pinMode(PIN_DCF77_TIME_IN, INPUT);
   Serial.begin(9600);
-  DCF.Start();
+}
+
+#define BUF_LEN 120
+volatile int buf[BUF_LEN];
+volatile int bufPos = 0;
+
+void waitFor(int level) {
+  while(digitalRead(PIN_DCF77_TIME_IN) == level) {
+    delayMicroseconds(50);
+  }
 }
 
 void loop()
 {
-  delay(1000);
-  time_t DCFtime = DCF.getTime();
-  if (DCFtime != 0)
-  {
-    Serial.println("Time is updated");
-    setTime(DCFtime);
-
-    // For ST-LINK + SWD debug
-    volatile int t_hour = 0;
-    volatile int t_minute = 0;
-    volatile int t_second = 0;
-    t_hour = hour();
-    Serial.println(t_hour);
-    t_minute = minute();
-    Serial.println(t_minute);
-    t_second = second();
-    Serial.println(t_second);
+  waitFor(HIGH);
+  uint32_t stime = millis();
+  waitFor(LOW);
+  int highTime = millis() - stime;
+  buf[bufPos] = highTime;
+  bufPos++;
+  if(bufPos >= BUF_LEN) {
+    bufPos = 0;
   }
 }
