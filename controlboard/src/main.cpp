e1a5ef58009c0604d551c73842f20c0bfed54a0f
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-09-07 19:17:19 +0200

    feat: wifi password provisioning

--- controlboard/src/main.cpp
+++ controlboard/src/main.cpp
@@ -1,4 +1,14 @@
 #include "Arduino.h"
+#include "secrets.h"
+#include <WiFi.h>
+
+#ifndef SECRET_WIFI_SSID
+#error You have to define SECRET_WIFI_SSID in secrets.h
+#endif
+
+#ifndef SECRET_WIFI_PASSWORD
+#error You have to define SECRET_WIFI_PASSWORD in secrets.h
+#endif
 
 void setup()
 {
@@ -13,4 +23,5 @@ void loop()
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
   Serial.println("Hello world");
+  Serial.printf("ssid=%s/n", SECRET_WIFI_SSID);
 }
