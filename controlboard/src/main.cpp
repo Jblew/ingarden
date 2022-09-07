856a40abe9cfed4bf4bbb417e71c0d28906f19a8
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-09-07 19:04:44 +0200

    feat: use Wemos D1 R32 board

--- controlboard/src/main.cpp
+++ controlboard/src/main.cpp
@@ -1,34 +1,16 @@
 #include "Arduino.h"
 
-#define PIN_INDICATOR_LED PC13
-#define PIN_DCF77_TIME_IN PB9
-
 void setup()
 {
-  pinMode(PIN_INDICATOR_LED, OUTPUT);
-  pinMode(PIN_DCF77_TIME_IN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-}
-
-#define BUF_LEN 120
-volatile int buf[BUF_LEN];
-volatile int bufPos = 0;
-
-void waitFor(int level) {
-  while(digitalRead(PIN_DCF77_TIME_IN) == level) {
-    delayMicroseconds(50);
-  }
 }
 
 void loop()
 {
-  waitFor(HIGH);
-  uint32_t stime = millis();
-  waitFor(LOW);
-  int highTime = millis() - stime;
-  buf[bufPos] = highTime;
-  bufPos++;
-  if(bufPos >= BUF_LEN) {
-    bufPos = 0;
-  }
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Hello world");
 }
